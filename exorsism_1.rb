6179c792ed4cda60a3f7d46b610d7459d7e75c81
glckfndr <obulan65@gmail.com>
2023-05-04 07:48:31 +0300

    Add min

--- exorsism_1.rb
+++ exorsism_1.rb
@@ -1,5 +1,6 @@
 require 'ostruct'
-attributes = {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: OpenStruct.new({s: 3, m: 7, l: 8, xl: 4}) }
+attributes = {price: 65.00, name: "Maxi Brown Dress", 
+      quantity_by_size: OpenStruct.new({s: 3, m: 7, l: 8, xl: 4}) }
 item = OpenStruct.new(attributes)
 
 puts item.price
